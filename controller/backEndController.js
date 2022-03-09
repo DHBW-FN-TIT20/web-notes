ec0f7d1f551ea2ebb33cdadc430cebb2d6c29f87
schuler-henry <72646334+schuler-henry@users.noreply.github.com>
2022-03-09 19:48:07 +0100

    Fixed merge error.

--- controller/backEndController.js
+++ controller/backEndController.js
@@ -308,6 +308,7 @@ export class BackEndController {
       console.log("note.sharedUserIDs: ", note.sharedUserIDs);
 
       // TODO: @schuler-henry evaluate success of update
+      if (note.sharedUserIDs !== undefined && savedNote.ownerID === user.id) {
         console.log(this.databaseModel.evaluateSuccess(await this.databaseModel.deleteUserNoteRelation(undefined, note.id)));
         if (note.sharedUserIDs.length > 0) {
           console.log(this.databaseModel.evaluateSuccess(await this.databaseModel.addUserNoteRelation(note.sharedUserIDs, note.id)));
